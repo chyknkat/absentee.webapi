1e5f74c64edb499ef04e00b53d140d5239a24a22
Katrina Ortiz <kortiz@instechnologies.net>
2017-09-21 14:50:36 -0400

    Change connection strings to point to dev database

--- Absentee.WebApi/Data/SessionFactory.cs
+++ Absentee.WebApi/Data/SessionFactory.cs
@@ -11,7 +11,8 @@ namespace Absentee.WebApi.Data
     {
         public static ISessionFactory BuildSessionFactory()
         {
-            var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Absentee;Integrated Security=True";
+            //var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Absentee;Integrated Security=True";
+            var connectionString = "Server=tcp:instechsqldev.database.windows.net,1433;Initial Catalog=Absentee;Persist Security Info=False;User ID=server-admin;Password=InsTech2016;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             var sessionFactory = Fluently.Configure()
                 .Database(MsSqlConfiguration.MsSql2012.ConnectionString(connectionString).Driver<SqlAzureClientDriver>().IsolationLevel(IsolationLevel.ReadUncommitted))
                 .Mappings(m =>
